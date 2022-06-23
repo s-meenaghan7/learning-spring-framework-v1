9c1e5e45de8d0532000c13a965eb1a27792874d2
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-06-22 18:24:40 -0700

    implemented ResponseHandler class

--- src/main/java/my/app/learningspringv1/response/ResponseHandler.java
+++ src/main/java/my/app/learningspringv1/response/ResponseHandler.java
@@ -1,2 +1,20 @@
-package my.app.learningspringv1.response;public class ResponseHandler {
+package my.app.learningspringv1.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus httpStatus, Object responseObj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", message);
+        map.put("status", httpStatus.value());
+        map.put("data", responseObj);
+
+        return new ResponseEntity<Object>(map, httpStatus);
+    }
+
 }
