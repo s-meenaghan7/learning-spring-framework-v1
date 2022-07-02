8a83dca54d225fb8bf33206057e30b0f9b4d1f16
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-07-01 18:34:09 -0700

    created custom exception, updates to controller and readme

--- src/main/java/my/app/learningspringv1/person/PersonController.java
+++ src/main/java/my/app/learningspringv1/person/PersonController.java
@@ -1,13 +1,14 @@
 package my.app.learningspringv1.person;
 
 import lombok.AllArgsConstructor;
+import my.app.learningspringv1.exception.ResourceNotFoundException;
+import my.app.learningspringv1.response.ResponseHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -18,16 +19,20 @@ public class PersonController {
     private final PersonService personService;
 
     @PostMapping("add")
-    public String addPerson(@RequestBody Person person) {
+    public ResponseEntity<Object> addPerson(@RequestBody Person person) {
         personService.addPerson(person);
-        return "Saved " + person.getName();
+        return ResponseHandler.generateResponse(
+                "Saved data successfully",
+                HttpStatus.OK,
+                person
+        );
     }
 
     @GetMapping("find")
-    public ResponseEntity<Person> findPersonById(@RequestParam("id") Integer id) {
+    public ResponseEntity<Object> findPersonById(@RequestParam("id") Integer id) throws ResourceNotFoundException {
         Optional<Person> person = personService.findPersonById(id);
-        return person.map(value -> ResponseEntity.ok().body(value))
-                  .orElseGet(() -> ResponseEntity.notFound().build());
+        return person.map(value -> ResponseHandler.generateResponse("Retrieved person of id " + id, HttpStatus.OK, person))
+                  .orElseThrow(() -> new ResourceNotFoundException("Could not find person of id " + id));
     }
 
     @GetMapping("find/all")
@@ -35,26 +40,27 @@ public class PersonController {
         return personService.findAllPersons();
     }
 
-    @PutMapping("update")
-    public String updatePersonById(@RequestParam("id") Integer id, @RequestBody Person person) {
-        if (personService.updatePerson(id, person))
-            return "Updated person of id=" + id + " successfully";
 
-//        return "No person of id=" + id + " found to update"; // testing purposes
-        throw new NoSuchElementException("No person of id=" + id + " found to update");
+    @PutMapping("update")
+    public ResponseEntity<Object> updatePersonById(@RequestParam("id") Integer id, @RequestBody Person person) throws ResourceNotFoundException {
+        if (personService.updatePerson(id, person)) {
+            person.setId(id);
+            return ResponseHandler.generateResponse("Updated person of id=" + id + " successfully", HttpStatus.OK, person);
+        }
+
+        throw new ResourceNotFoundException("No person of id " + id + " found to update.");
     }
 
     @DeleteMapping("delete")
-    public String deletePersonById(@RequestParam("id") Integer id) {
+    public ResponseEntity<Object> deletePersonById(@RequestParam("id") Integer id) throws ResourceNotFoundException {
         if (personService.deletePersonById(id))
-            return "Deleted person of id=" + id + " successfully";
+            return ResponseHandler.generateResponse("Deleted person of id=" + id + " successfully", HttpStatus.OK, null);
 
-//        return "No person of id=" + id + " found to delete"; // testing purposes
-        throw new NoSuchElementException("No person of id=" + id + " found to delete");
+        throw new ResourceNotFoundException("No person of id " + id + " found to delete.");
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleMissingRequestBody(HttpMessageNotReadableException ex) {
-        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
